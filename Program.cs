d63c7f6a30978d5dc4efe7f0181688160a996a08
Ali Aliyev <elvinaliyev724@gmail.com>
2024-03-14 11:16:23 +0400

    readme

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3._14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    int[] nums = { 12, 43, 24, 36, 47, 94, 88 };
+            //    Console.WriteLine("Yas daxil edin");
+            //    int age = Convert.ToInt32(Console.ReadLine());
+            //    int age1 = 0;
+            //    for (int i = 0; i < nums.Length; i++)
+            //    {
+            //        if (age == nums[i])
+            //        {
+            //            age1++;
+            //        }
+            //    }
+            //    if (age1 == 0)
+            //    {
+            //        Console.WriteLine(false);
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(true);
+            //    }
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("Yasi duzgun daxil edin");
+            //}
+
+
+
+            //Code:
+
+            //string[] names = { "Namiq", "Cavid", "Ceyhun", "Mehman", "Elvin", "Huseyin" };
+            //Console.WriteLine("Ad daxil edin");
+            //string name = Console.ReadLine();
+            //bool check = char.TryParse(name, out char a1);
+           
+            
+            //string name1 = "";
+           
+            //foreach (var item in names)
+            //{
+                
+            //    if (name == item)
+            //    {
+            //        name1 += name;
+
+            //    }
+
+            //}
+            //if (name != name1)
+            //{
+            //    if (!check)
+            //    {
+            //        Console.WriteLine("Ad duzgun daxil edin");
+            //        goto Code;
+            //    }
+            //    Console.WriteLine("Bu adda telebe massivde tapilmadi");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Telebe massivde tapildi");
+            //    Console.WriteLine(name1);
+            //}
+
+
+
+
+        }
+    }
+}
